fee1bb145b19dbf9b1b91179577700b17df65962
sa-e <santiagoemenss@gmail.com>
2023-05-02 20:01:17 -0300

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import productosjson from 'src/assets/json/productos.json'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lodemarta';
+
+  productos: any = productosjson
 }
