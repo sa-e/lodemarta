169184edb573a044a9fda331009ae926604b404a
sa-e <santiagoemenss@gmail.com>
2023-05-06 19:39:24 -0300

    1

--- src/app/carta/carta.component.ts
+++ src/app/carta/carta.component.ts
@@ -18,7 +18,7 @@ export class CartaComponent {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get<Producto[]>('assets/json/productos.json')
+    this.http.get<Producto[]>('https://raw.githubusercontent.com/sa-e/lodemarta/main/src/assets/json/productos.json')
     .pipe(
       map(data => data.map(item => ({nombre: item.nombre, precio: item.precio})))
     )
